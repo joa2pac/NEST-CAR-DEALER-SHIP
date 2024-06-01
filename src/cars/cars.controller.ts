b7640eae523b6c0f5e61c872e5448a10255eb8bf
joaquin <joaquinahuel93@gmail.com>
2024-06-01 20:35:04 -0300

    add validation pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -9,6 +9,8 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,5 +1,8 @@
-export class CreateCarDto {
-  readonly brand: string;
+import { IsString } from 'class-validator';
 
+export class CreateCarDto {
+  @IsString()
+  readonly brand: string;
+  @IsString()
   readonly model: string;
 }
