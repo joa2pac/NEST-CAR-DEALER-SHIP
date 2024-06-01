29919662b016fd8804820ce6dbae09cf20ae4970
joaquin <joaquinahuel93@gmail.com>
2024-06-01 19:59:50 -0300

    add createCardto

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -11,6 +11,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -28,8 +29,8 @@ export class CarsController {
   }
 
   @Post()
-  createCar(@Body() body: any) {
-    return body;
+  createCar(@Body() createCarDto: CreateCarDto) {
+    return createCarDto;
   }
 
   @Patch(':id')
