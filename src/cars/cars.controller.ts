c6cb46d2b8d584aa7d3fa8bc8ac556577773a8b3
joaquin <joaquinahuel93@gmail.com>
2024-06-02 20:34:28 -0300

    add update car service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -41,7 +41,7 @@ export class CarsController {
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCarDto: UpdateCarDto,
   ) {
-    return updateCarDto;
+    return this.carsService.updateCar(id, updateCarDto);
   }
 
   @Delete(':id')
